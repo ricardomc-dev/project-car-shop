c5ca8896ad7a74b7414904b0e25b9e333d657e66
Ricardo Carvalho <ricardomc.dev@gmail.com>
2022-08-30 16:44:34 -0300

    chore: add docker-compose

--- app/backend/.eslintrc.js
+++ app/backend/.eslintrc.js
@@ -3,13 +3,17 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: 'standard-with-typescript',
-  overrides: [
+  extends: [
+    'airbnb-base',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
-  }
-}
+  },
+};
